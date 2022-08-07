ad9b7443e4c577d93f7d8f6ffda5e639789d7341
Nguyễn Hữu Thắng <84012140+nguyenhuuthang113@users.noreply.github.com>
2022-08-08 01:02:50 +0700

    ok
    ok

---
+++ public/js/config.js
@@ -0,0 +1,60 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'VIỆT NAM BẾP',
+        'favicon' : 'home.png',
+        'notice': '🤗Welcome To Vietnambep.vn😘'
+    },
+    'cover-image' : 'vietnambep.gif',
+    'avatar-image' : 'avatar.jpg',
+    'name' : 'Việt Nam Bếp', 
+    'is_tick' : true,
+    'skills' : [
+        'Developer',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'admin.png',
+            'content' : 'LIÊN HỆ 👇',
+            'author' : 'PHẠM ANNA',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'PHẠM THỊ AN NA',
+            'value' : 'https://chanlemomo.store/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook Quản Lý', 
+            'show-value' : 'An Na' ,
+            'value' : 'https://www.facebook.com/profile.php?id=100007048219029',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo Quản Lý', 
+            'show-value' : 'Phamanna',
+            'value' : 'https://zalo.me/0967352113',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Số Điện Thoại Quản Lý', 
+            'show-value' : '0967352113',
+            'value' : 'tel:0967352113',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'Anna@vietnambep.vn',
+            'value' : 'mailto:anna@vietnambep.vn',
+            'type' : 0,
+        },
+    ]
+};
+
