aeb365a473d120e89c3ad797e514fe6075457096
Nguyễn Hữu Thắng <84012140+nguyenhuuthang113@users.noreply.github.com>
2022-08-10 13:56:40 +0700

    ok

---
+++ banking/public/js/config.js
@@ -0,0 +1,68 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'VIỆT NAM BẾP|Phạm Anna',
+        'favicon' : 'home.png',
+        'notice': '🤗Welcome To Vietnambep.vn😘'
+    },
+    'cover-image' : 'vietnambep.gif',
+    'avatar-image' : 'avatar.jpg',
+    'name' : 'Việt Nam Bếp', 
+    'is_tick' : true,
+    'skills' : [
+        'Developer',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'admin.png',
+            'content' : 'LIÊN HỆ 👇',
+            'author' : 'PHẠM ANNA',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'catalogue.jpg',
+            'title' : 'CATALOGUE', 
+            'show-value' : 'Liên Kết Driver Catalogue',
+            'value' : 'https://drive.google.com/file/d/16F29m8UftuVBgGBMsceuqpCDXcwYpjyD/view?usp=drivesdk',
+            'type' : 0,
+        },
+        
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Fanpage', 
+            'show-value' : 'Việt Nam Bếp' ,
+            'value' : 'https://www.facebook.com/108400174989955/',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo Quản Lý', 
+            'show-value' : 'Phamanna',
+            'value' : 'https://zalo.me/0967352113',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Số Điện Thoại Quản Lý', 
+            'show-value' : '0967352113',
+            'value' : 'tel:0967352113',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'Anna@vietnambep.vn',
+            'value' : 'mailto:anna@vietnambep.vn',
+            'type' : 0,
+        },
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'PHẠM THỊ AN NA',
+            'value' : 'https://nguyenhuuthang113.github.io/vietnambep/banking',
+            'type' : 0,
+        },
+    ]
+};
+
